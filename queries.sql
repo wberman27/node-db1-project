f72c52182720f596bac3f17570aac430991b04fd
Will B <wberman27@gmail.com>
2021-04-12 17:56:46 -0400

    ran npm i, added half of SQL queries

--- queries.sql
+++ queries.sql
@@ -1,13 +1,13 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
-
+SELECT * FROM Customers where postalcode = 1010;
 -- Find the phone number for the supplier with the id 11
-
+SELECT Phone FROM Suppliers where SupplierID = 11
 -- List first 10 orders placed, sorted descending by the order date
-
+SELECT * from orders order by OrderDate desc limit 10
 -- Find all customers that live in London, Madrid, or Brazil
-
+select * from customers where city in ("London", "Madrid") or country = "Brazil";
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
