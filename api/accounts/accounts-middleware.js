918b10edfde9bd8591842e8efdf1f187710a96eb
Will B <wberman27@gmail.com>
2021-04-12 20:20:51 -0400

    added checkaccountpayload middleware

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,7 +1,31 @@
 const Accounts = require('./accounts-model')
 
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  try{
+    if(!req.body.name || !req.body.budget){
+      res.status(400).json({ message: "name and budget are required" })
+    }else{
+      if(typeof req.body.name !== "string"){
+        res.status(400).json({ message: "name of account must be a string" })
+      }else{
+        if(req.body.name.trim().length < 3 || req.body.name.trim().length > 100){
+          res.status(400).json({ message: "name of account must be between 3 and 100" })
+        }else{
+          if(typeof req.body.budget !== "number"){
+            res.status(400).json({ message: "budget of account must be a number" })
+          }else{
+            if(req.body.budget < 0 || req.body.budget > 1000000){
+              res.status(400).json({ message: "budget of account is too large or too small" })
+            }else{
+              next()
+            }
+          }
+        }
+      }
+    }
+  }catch(err){
+    next(err)
+  }
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
